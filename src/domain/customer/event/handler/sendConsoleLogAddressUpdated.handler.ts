596c7b0e6f46037b68e1b8f270f101b97a6c8060
debora-cristinaf <programacaodcrisf@gmail.com>
2023-02-27 13:06:44 -0300

    fix: tests

--- src/domain/customer/event/handler/sendConsoleLogAddressUpdated.handler.ts
+++ src/domain/customer/event/handler/sendConsoleLogAddressUpdated.handler.ts
@@ -1,12 +1,15 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import CustomerCreatedEvent from "../customer-created.event";
+import { CustomerChangedAddressEvent } from "../customer-updated.event";
 
 export default class SendConsoleLogAddressHandler
   implements EventHandlerInterface<CustomerCreatedEvent>
 {
-  handle(event: CustomerCreatedEvent): void {
+  handle(event: CustomerChangedAddressEvent): void {
+    const { id, name, oldAddress, newAddress } = event.payload.customer;
     console.log(
-      `Endereço do cliente: ${event.eventData?.id}, ${event.eventData?.name} alterado para: ${event.eventData?.address}`
+      `Endereço do cliente: ${id}, ${name} , endereço: ${oldAddress.street}, ${oldAddress.city}, ${oldAddress.number}, ${oldAddress.postalCode}
+       alterado para: ${newAddress.street}, ${newAddress.city}, ${newAddress.number}, ${newAddress.postalCode}`
     );
   }
 }
